5de89129c6ca207e0385bf319b8b0780d353ce74
Mark <14284867+xHeaven@users.noreply.github.com>
2023-10-11 01:43:44 +0200

    add message if exists

--- src/DataTransferObjects/Client/ClientDelete.php
+++ src/DataTransferObjects/Client/ClientDelete.php
@@ -6,8 +6,11 @@ class ClientDelete
 {
     public string $status;
 
+    public ?string $message;
+
     public function __construct(object $response)
     {
         $this->status = $response->status;
+        $this->message = $response->message ?? null;
     }
 }
