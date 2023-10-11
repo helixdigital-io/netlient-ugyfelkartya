295d186354c740acaad33d34173741b528a20f81
Mark <14284867+xHeaven@users.noreply.github.com>
2023-10-11 14:11:51 +0200

    create a php 8.0+ version of the package

--- src/Ugyfelkartya.php
+++ src/Ugyfelkartya.php
@@ -2,114 +2,34 @@
 
 namespace HelixdigitalIo\NetlientUgyfelkartya;
 
-use GuzzleHttp\Client;
-use HelixdigitalIo\NetlientUgyfelkartya\DataTransferObjects\Card as CardDto;
-use HelixdigitalIo\NetlientUgyfelkartya\DataTransferObjects\Client\ClientDelete as ClientDeleteDto;
-use HelixdigitalIo\NetlientUgyfelkartya\DataTransferObjects\ClientInfo as ClientInfoDto;
-use HelixdigitalIo\NetlientUgyfelkartya\DataTransferObjects\Registration as RegistrationDto;
-use HelixdigitalIo\NetlientUgyfelkartya\Traits\IsSuccessfulResponse;
-use HelixdigitalIo\NetlientUgyfelkartya\Traits\SetErrorData;
+use HelixdigitalIo\NetlientUgyfelkartya\Requests\Card\GetCard;
+use HelixdigitalIo\NetlientUgyfelkartya\Requests\Client\ClientDelete;
+use HelixdigitalIo\NetlientUgyfelkartya\Requests\ClientInfo;
+use HelixdigitalIo\NetlientUgyfelkartya\Requests\Registration;
+use HelixdigitalIo\NetlientUgyfelkartya\Responses\Card\GetCard as GetCardResponse;
+use HelixdigitalIo\NetlientUgyfelkartya\Responses\Client\ClientDelete as ClientDeleteResponse;
+use HelixdigitalIo\NetlientUgyfelkartya\Responses\ClientInfo as ClientInfoResponse;
+use HelixdigitalIo\NetlientUgyfelkartya\Responses\Registration as RegistrationResponse;
 
 class Ugyfelkartya
 {
-    use IsSuccessfulResponse, SetErrorData;
-
-    private Client $client;
-    private string $baseUrl = 'https://www.ugyfelkartya.hu/api';
-
-    /** @var array<string, string> */
-    private array $headers = [
-        'Content-Type' => 'application/json;charset=UTF-8',
-        'Accept' => 'application/json',
-    ];
-
-    public function __construct() {
-        $this->client = new Client([
-            'base_uri' => $this->baseUrl,
-            'headers' => $this->headers,
-            'auth' => [
-                $_ENV['API_USERNAME'],
-                $_ENV['API_PASSWORD']
-            ]
-        ]);
-    }
-
-    public function register(
-        int $storeId,
-        string $email,
-        string $firstName,
-        string $lastName,
-        ?string $birthDate,
-        ?string $zipCode
-    ): ?RegistrationDto
+    public static function register(int $storeId, string $email, string $firstName, string $lastName, ?string $birthDate): RegistrationResponse
     {
-        $body = [
-            'store_id' => $storeId,
-            'e_mail' => $email,
-            'first_name' => $firstName,
-            'last_name' => $lastName,
-            'activate_card' => true,
-            'get_virtualcard' => true,
-        ];
-
-        if (!empty($birthDate)) {
-            $body['birthdate'] = $birthDate;
-        }
-
-        if (!empty($zipCode)) {
-            $body['zip_code'] = $zipCode;
-        }
-
-        $jsonResponse = (string)($this->client->post('/registration', [
-            'json' => $body
-        ])->getBody());
-
-        $response = $this->parseResponse($jsonResponse);
-
-        return new RegistrationDto($response);
+        return (new Registration($storeId, $email, $firstName, $lastName, $birthDate))->send();
     }
 
-    public function getClientInfo(string $email): ?ClientInfoDto
+    public static function getClientInfo(string $email): ClientInfoResponse
     {
-        $jsonResponse = (string)($this->client->get("/clientinfo?email={$email}")->getBody());
-
-        $response = $this->parseResponse($jsonResponse);
-
-        return new ClientInfoDto($response);
+        return (new ClientInfo($email))->send();
     }
 
-    public function getCard(string $cardNumber): ?CardDto
+    public static function getCard(string $cardNumber): GetCardResponse
     {
-        $jsonResponse = (string)($this->client->get("/card/{$cardNumber}")->getBody());
-
-        $response = $this->parseResponse($jsonResponse);
-
-        return new CardDto($response);
+        return (new GetCard($cardNumber))->send();
     }
 
-    public function deleteClient(int $clientId, int $storeId): ?ClientDeleteDto
+    public static function deleteClient(int $clientId, int $storeId): ClientDeleteResponse
     {
-        $jsonResponse = (string)($this->client->post('/client/delete', [
-            'json' => [
-                'client_id' => $clientId,
-                'store_id' => $storeId,
-            ]
-        ])->getBody());
-
-        $response = $this->parseResponse($jsonResponse);
-
-        return new ClientDeleteDto($response);
-    }
-
-    private function parseResponse(string $jsonResponse) {
-        $response = json_decode($jsonResponse, false, 512, JSON_THROW_ON_ERROR);
-
-        if (!$this->isSuccessfulResponse($response)) {
-            $this->setErrorData($response);
-
-            return null;
-        }
-
-        return $response;
+        return (new ClientDelete($clientId, $storeId))->send();
     }
 }
